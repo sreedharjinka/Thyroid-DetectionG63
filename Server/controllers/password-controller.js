bfe8651076ead803ddab0c52e5f6ef9a92e7f64a
Jayanth-137 <jayanth.karnati2003@gmail.com>
2024-02-01 16:49:56 +0530

    ADDED SERVER

---
+++ Server/controllers/password-controller.js
@@ -0,0 +1,71 @@
+const User = require("../models/user-model");
+
+//Reset Password Logic
+const resetPwd = async (req, res)=>{
+    try {
+        const {Password, NewPassword, ConfirmPassword} = req.body;
+        const userData = req.user;
+        const isPwdValid = await userData.PasswordCheck(currPwd);
+        if(NewPassword==ConfirmPassword){
+            res.status(400).json({msg:"Both Passwords cannot be same"});
+        }
+        if(!isPwdValid){
+            res.status(400).json({msg:"Invalid Password."});
+        }
+        if(newPwd!=confirmPwd){
+            res.status(400).json({msg:"Passwords don't match."});
+        }
+        userData.password=newPwd;
+        await userData.save();
+        res.status(200).json({msg:"Password changed successfully."});
+    } catch (error) {
+        console.log("Error in Reset password",error);
+        res.status(500).json({msg:"Internal Server Error."});
+    }
+}
+
+//Forgot Password Logic
+const forgotpwd = async (req, res)=>{
+    try {
+        const {email} = req.body;
+        const userData = req.user;
+        if(userData.email!=email){
+            return res.status(400).json({msg:"Please enter correct email"})
+        }
+        var token =await userData.generateToken();
+        try {
+            await sendResetMail(email,token);
+            res.status(200).json({msg:"Email Sent Successfully."});
+        } catch (error) {
+            res.status(400).json({msg:"Unable to sent the email."});
+        }
+        
+    } catch (error) {
+        console.log(`error in Reset pwd route ${error}`);
+    }
+}
+const jwt = require('jsonwebtoken');
+const forgotPwdemail = async (req, res)=>{
+    try {
+        const resetToken = req.params.token;
+        const resetEmail = req.params.email;
+        const {newPwd,confirmNewPwd} = req.body;
+        const dataVerify = jwt.verify(resetToken,process.env.JWT_KEY);
+        if(dataVerify.email!=resetEmail){
+            res.status(400).json({msg:"Invalid Link"});
+        }
+        if(newPwd!=confirmNewPwd){
+            res.status(400).json({msg:"Passwords don't match"});
+        }
+        console.log("Valid URL!!");
+        const user =await User.findOne({email:dataVerify.email});
+        user.password=newPwd;
+        user.save();
+        res.status(200).json({});
+    } catch (error) {
+        res.status(400).json("Invalid Link");
+        console.log("Error in reset Password.");
+    }
+}
+
+module.exports = { resetPwd,forgotpwd, forgotPwdemail };
